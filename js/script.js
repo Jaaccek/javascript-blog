d69b94614be66860291da9b77f592fcee9d8de0b
Jacek Knapczyk <jacekknapczyk@gmail.com>
2021-06-15 22:45:10 +0200

    one more

--- js/script.js
+++ js/script.js
@@ -11,13 +11,11 @@
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    
-    
-    
+    console.log(event);
     
     /* [DONE] remove class 'active' from all article links  */
 
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
   
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
@@ -25,13 +23,13 @@
 
     /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active');{
+    clickedElement.classList.add('active');
     
     console.log('clickedElement:', clickedElement);
-    }
+    
     /* [DONE] remove class 'active' from all articles */
   
-  const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('article.active');
     
     for (let activeArticle of activeArticles){
      
@@ -40,13 +38,13 @@
 
     /* get 'href' attribute from the clicked link */
        
-  const articleSelector = clickedElement.getAttribute('href');{
+    const articleSelector = clickedElement.getAttribute('href');
         
     console.log('articleSelector:', articleSelector);
-    }
+    
     /* find the correct article using the selector (value of 'href' attribute) */
     
-  const targetArticle = document.querySelector('articleSelector');
+    const targetArticle = document.querySelector(articleSelector);
 
     console.log('targetArticle', targetArticle);
     
@@ -55,10 +53,11 @@
     targetArticle.classList.add('active');
   
     console.log(targetArticle);
-  
-  const links = document.querySelectorAll('.titles a');
-
-    for(let link of links){
-    link.addEventListener('click' , titleClickHandler);
-    }
   }
+
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click' , titleClickHandler);
+  }
+  
